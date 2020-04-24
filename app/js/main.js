ccfbdb7038d75ae36cc9b779daeef11c7b447680
nikitkamatveev <nikitkamatveev@icloud.com>
2020-04-24 23:54:57 +0300

    added pr slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function() {
 
+    $(".rate-start").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+    });
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
 
-
-
+    });
+    var mixer = mixitup('.products__inner-box');
 
 });
